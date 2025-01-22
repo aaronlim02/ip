6162e0df6ec56f57d8f00249dc54d6e2a2de7f55
aaronlim02 <e1123168@u.nus.edu>
2025-01-22 20:21:31 +0800

    completed Level 1. Echo

--- src/main/java/NotChatGPT.java
+++ src/main/java/NotChatGPT.java
@@ -1,4 +1,6 @@
+import java.util.Scanner;
 public class NotChatGPT {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,10 +8,20 @@ public class NotChatGPT {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String name = "Not ChatGPT";
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
         System.out.println();
+        while (true) {
+            String echo = sc.nextLine();
+            if (echo.equals("bye")) {
+                break;
+            }
+            System.out.println(echo);
+            System.out.println();
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
