3f576051e1e821e1b59716966103daa3c909f098
aaronlim02 <e1123168@u.nus.edu>
2025-01-23 15:27:46 +0800

    completed Level 3. Mark as Done

--- src/main/java/NotChatGPT.java
+++ src/main/java/NotChatGPT.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class NotChatGPT {
 
     public static void main(String[] args) {
-        ArrayList<String> tasklist = new ArrayList<>();
+        ArrayList<Task> tasklist = new ArrayList<>();
         String name = "Not ChatGPT";
         Scanner sc = new Scanner(System.in);
 
@@ -15,14 +15,29 @@ public class NotChatGPT {
                 break;
             } else if (echo.equals("list")) {
                 for (int i = 0; i < tasklist.toArray().length; i++) {
-                    System.out.println(i+1 + ". " + tasklist.get(i));
+                    System.out.println(i+1 + ".[" + tasklist.get(i).getStatusIcon()
+                        + "] " + tasklist.get(i).getDescription());
                 }
-                System.out.println();
+            } else if (echo.substring(0, Math.min(echo.length(), 4)).equals("mark")) {
+                // change event status to 'X'
+                int id = Integer.parseInt(echo.substring(5)) - 1;
+                tasklist.get(id).setStatusIcon('X');
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  [" + tasklist.get(id).getStatusIcon()
+                    + "] " + tasklist.get(id).getDescription());
+            } else if (echo.substring(0, Math.min(echo.length(), 6)).equals("unmark")) {
+                // change event status to ' '
+                int id = Integer.parseInt(echo.substring(7)) - 1;
+                tasklist.get(id).setStatusIcon(' ');
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  [" + tasklist.get(id).getStatusIcon()
+                    + "] " + tasklist.get(id).getDescription());
             } else {
                 System.out.println("added:" + echo + '\n');
-                tasklist.add(echo);
+                tasklist.add(new Task(echo));
             }
 
+            System.out.println();
 
         }
 
